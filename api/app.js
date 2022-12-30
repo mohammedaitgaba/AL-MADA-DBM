44420da7984a9326fe2790ad6a20941dec242a74
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2022-12-30 10:08:58 +0100

    add Graphql with simple test

--- api/app.js
+++ api/app.js
@@ -3,7 +3,10 @@ const cors = require('cors')
 const mongoose = require('mongoose');
 const app = express()
 require("dotenv").config();
-
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+const fs = require('fs');
+const csv = require("csv-parser");
 const connectDB = require('./src/config/db.config')
 connectDB()
 const port = process.env.PORT
@@ -12,23 +15,104 @@ mongoose.set('strictQuery', false);
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.json([
-    {
-      "id":"1",
-      "title":"Book Review: The Bear & The hound"
-    },
-    {
-      "id":"2",
-      "title":"Game Review: Pokemon Brillian Diamond"
-    },
-    {
-      "id":"3",
-      "title":"Show Review: Alice in Borderland"
+const fakeDatabase = {};
+// Define your GraphQL schema
+const schema = buildSchema(`
+  # A message object with a string field
+  type Message {
+    id: ID!
+    content: String
+  }
+  type Users {
+    id:ID!
+    Fname:String
+    Lname:String
+  }
+
+  # The query type, with a field to retrieve a message by ID
+  type Query {
+    message(id: ID!): Message
+    users(id: ID!): Users
+  }
+
+  # The mutation type, with fields to create, update, and delete messages
+  type Mutation {
+    importData:String
+    createMessage(input: String!): Message
+    createUser(Fname: String!,Lname: String!): Users
+    updateMessage(id: ID!, input: String!): Message
+    deleteMessage(id: ID!): Message
+  }
+`);
+// Define your GraphQL resolvers
+
+
+const importDataFromGBD =()=>{
+  fs.createReadStream('GBD-sidiali.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+      MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+          if (err) {
+              console.log(err);
+              return;
+          }else{
+          const db = client.db("AL-MADA-DBM");
+          db.collection("test").insertOne(row, function(err, result) {
+          client.close();
+          });
+          }
+      });
+      }).on('end', () => {
+          console.log('CSV file successfully processed');
+      });
+  return "Data imported";
+}
+const root = {
+  importData:importDataFromGBD,
+  message: ({ id }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error(`no message exists with id ${id}`);
     }
-  ])
-})
-app.use('/api/users',require('./src/routes/test.routes'))
+    return fakeDatabase[id];
+  },
+  users: ({ id }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error(`no message exists with id ${id}`);
+    }
+    return fakeDatabase[id];
+  },
+  createMessage: ({ input }) => {
+    const id = require('crypto').randomBytes(10).toString('hex');
+    fakeDatabase[id] = { id, content: input };
+    return fakeDatabase[id];
+  },
+
+  createUser: ({ Fname,Lname }) => {
+    const id = require('crypto').randomBytes(10).toString('hex');
+    fakeDatabase[id] = { id, Fname: Fname,Lname: Lname };
+    return fakeDatabase[id];
+  },
+  updateMessage: ({ id, input }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error(`no message exists with id ${id}`);
+    }
+    fakeDatabase[id].content = input;
+    return fakeDatabase[id];
+  },
+  deleteMessage: ({ id }) => {
+    const message = fakeDatabase[id];
+    delete fakeDatabase[id];
+    return message;
+  },
+};
+
+
+// Use the express-graphql middleware to expose your GraphQL API
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.listen(port, () => {
   console.log('listening for requests on port :'+port)
